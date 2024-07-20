18cf57d9564f8757d7e5eee331d3c532f07387f6
KhoaAnhDoNguyen <dokhoahay1@gmail.com>
2024-07-20 16:04:36 +0700

    Logic for Seeder, Factory and Faker

---
+++ laravel-test/database/factories/CustomerFactory.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Customer;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+class CustomerFactory extends Factory
+{
+    protected $model = Customer::class;
+
+    public function definition()
+    {
+        return [
+            'username' => $this->faker->userName,
+            'password' => bcrypt($this->faker->password),
+        ];
+    }
+}
