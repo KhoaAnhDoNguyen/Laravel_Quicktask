608e7113260986dfe46f2f0abf45a8c5877936ab
KhoaAnhDoNguyen <dokhoahay1@gmail.com>
2024-07-22 12:45:54 +0700

    Handle logic change languege

---
+++ laravel-test/app/Http/Middleware/Localization.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Session;
+use Symfony\Component\HttpFoundation\Response;
+
+class Localization
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $locale = Session::get('locale') ?? 'en';
+        Session::put('locale', $locale);
+        App::setlocale($locale);
+        return $next($request);
+    }
+}
